3abd674e47b4ce55d14a66a50cf484783af9fb98
Nathan Shirley <natethegreat2525@gmail.com>
2017-01-03 21:15:27 -0500

    add hash code to vector3i

--- src/com/nshirley/engine3d/math/Vector3i.java
+++ src/com/nshirley/engine3d/math/Vector3i.java
@@ -2,6 +2,34 @@ package com.nshirley.engine3d.math;
 
 public class Vector3i {
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		result = prime * result + z;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vector3i other = (Vector3i) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		if (z != other.z)
+			return false;
+		return true;
+	}
+
 	public int x, y, z;
 	
 	public Vector3i() {
